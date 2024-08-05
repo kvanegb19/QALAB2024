f06ece8776218fb28077fe5204b77451dd1148a9
Vane <guevara16_vanessa@hotmail.com>
2024-08-05 11:23:27 -0500

    Guardando cambios en Main.java

--- AppArrays/src/Main.java
+++ AppArrays/src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
 
-        String configTest[] = {"123.123.42.12", "jkbe", "21231"};
-        String configProd[] = {"www.google.com", "jkbenites", "alalals"};
-        String configDesa[] = {"123.123.42.10", "userdesa02", "1234"};
+        String configTest[] = { "123.123.42.12", "jkbe", "21231" };
+        String configProd[] = { "www.google.com", "jkbenites", "alalals" };
+        String configDesa[] = { "123.123.42.10", "userdesa02", "1234" };
 
         int XPROD = 2;
         int XTEST = 1;
@@ -14,21 +14,19 @@ public class Main {
         int CAMB = 3;
 
         String config[][] = {
-                {"123.123.42.10", "userdesa02", "1234", "DESA", "otro campo"},   //0
-                {"123.123.42.12", "jkbe", "21231", "TEST"},        //1
-                {"www.google.com", "jkbenites", "alalals", "PROD"} //2
+                { "123.123.42.10", "userdesa02", "1234", "DESA", "otro campo" }, // 0
+                { "123.123.42.12", "jkbe", "21231", "TEST" }, // 1
+                { "www.google.com", "jkbenites", "alalals", "PROD" } // 2
         };
 
+        // Hacer doble bucle for/for
+        System.out.println(config[XTEST][CAMB]);
 
-        //Hacer doble bucle for/for
+        // CLAVE: DESARROLLO
 
-
-        //CLAVE: DESARROLLO
-
-        //System.out.println(config[3][1]);
-        //System.out.println(config[1][1]);
-        System.out.println(config[XPROD][CPASS]); //...
-
+        // System.out.println(config[3][1]);
+        // System.out.println(config[1][1]);
+        System.out.println(config[XPROD][CPASS]); // ...
 
     }
 }
